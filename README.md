cab87155eaaa045fbfcc35eb025d4172fd299126
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2021-08-22 15:36:43 -0700

    fix some css and render options

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # grandpa
+
+Transliteration and annotation of my grandfather (Robert Preston "RP" Phillips)' journals kept during World War II.
+
+### TODO:
+[ ] xml to plain text ("as-is")
+[ ] xml to plain text (edited).
--- render.sh
+++ render.sh
@@ -1,1 +1,13 @@
-open -a Firefox Diary_II_12-13-43_03-27-44.xml
+VOLUME=${1:-I}
+
+if [ "$VOLUME" == I ]; then
+    FILE=Diary_I_6-13-43_12-12-43.xml
+elif [ "$VOLUME" == II ]; then
+    FILE=Diary_II_12-13-43_03-27-44.xml
+else
+    echo "Bad volume input.  Usage: render VOLUME"
+    exit
+fi
+
+echo Path: $FILE
+open -a Firefox text/$FILE
